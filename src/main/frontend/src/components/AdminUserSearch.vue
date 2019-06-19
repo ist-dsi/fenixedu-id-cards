b68aa7ebe0218fa5bb1cb422e55a14985a0920b6
tiagoppinho <tiagoppinho@gmail.com>
2019-06-20 00:35:27 +0100

    Add AdminUserSearch component for later usage

---
+++ src/main/frontend/src/components/AdminUserSearch.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="user-search">
+    <input
+      v-model="bindUsername"
+      :placeholder="$t('placeholder.search.user')"
+      @keyup.enter="goToUserPage" >
+    <button
+      class="btn btn--primary btn--outline"
+      @click.prevent="goToUserPage">
+      {{ $t('btn.search') }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AdminUserSearchPage',
+  props: {
+    username: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      bindUsername: ''
+    }
+  },
+  mounted () {
+    this.bindUsername = this.username
+  },
+  methods: {
+    goToUserPage () {
+      if (this.bindUsername) {
+        this.$router.push({ name: 'AdminViewUserCardsPage', params: { username: this.bindUsername } })
+      } else {
+        this.$router.push({ name: 'ListCardsPage' })
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .user-search {
+    display: flex;
+    & > input {
+      margin-right: 10px;
+    }
+  }
+</style>
