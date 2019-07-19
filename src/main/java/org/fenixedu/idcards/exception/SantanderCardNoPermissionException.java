3e7e451168a50fbf9cc708ae8e145eeb929bf135
João Abrantes <17602797+JCAbrantes@users.noreply.github.com>
2019-07-19 17:59:02 +0100

    Add SantanderCardNoPermissionException (#52)

---
+++ src/main/java/org/fenixedu/idcards/exception/SantanderCardNoPermissionException.java
@@ -0,0 +1,14 @@
+package org.fenixedu.idcards.exception;
+
+import org.fenixedu.santandersdk.exception.SantanderValidationException;
+
+public class SantanderCardNoPermissionException extends SantanderValidationException {
+
+    public SantanderCardNoPermissionException() {
+        super();
+    }
+
+    public SantanderCardNoPermissionException(String msg) {
+        super(msg);
+    }
+}
