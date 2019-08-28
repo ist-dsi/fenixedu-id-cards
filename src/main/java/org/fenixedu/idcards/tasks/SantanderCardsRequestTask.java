867d79c7cfc0b0160f02a9b99d2e3d2f8904333e
JCAbrantes <joaocarlos.abrantes@gmail.com>
2019-08-28 14:07:01 +0100

    Create crontask to issue first IST card

--- src/main/java/org/fenixedu/idcards/tasks/SantanderCardsRequestTask.java
+++ src/main/java/org/fenixedu/idcards/tasks/SantanderCardsRequestTask.java
@@ -1,49 +1,84 @@
 package org.fenixedu.idcards.tasks;
 
-import java.util.List;
-import java.util.Set;
+import java.util.Locale;
 
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.scheduler.CronTask;
+import org.fenixedu.bennu.scheduler.annotation.Task;
 import org.fenixedu.bennu.spring.BennuSpringContextHelper;
+import org.fenixedu.idcards.domain.SantanderEntry;
+import org.fenixedu.idcards.exception.SantanderCardNoPermissionException;
+import org.fenixedu.idcards.notifications.CardNotifications;
 import org.fenixedu.idcards.service.SantanderIdCardsService;
 import org.fenixedu.santandersdk.dto.RegisterAction;
+import org.fenixedu.santandersdk.exception.SantanderMissingInformationException;
 import org.fenixedu.santandersdk.exception.SantanderValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
 
+@Task(englishTitle = "Requests users first IST cards", readOnly = true)
 public class SantanderCardsRequestTask extends CronTask {
+
     private static final Logger logger = LoggerFactory.getLogger(SantanderCardsRequestTask.class);
+    private SantanderIdCardsService idCardsService = BennuSpringContextHelper.getBean(SantanderIdCardsService.class);
+    private final int waitTime = 500;   // TODO: check santander request rate
+    private static boolean waitFlag = false; //If the webservice is invoked we must wait calling before  it again
 
     @Override
     public Atomic.TxMode getTxMode() {
-        return Atomic.TxMode.WRITE;
+        return Atomic.TxMode.READ;
     }
 
     @Override
-    public void runTask() throws Exception {
-        SantanderIdCardsService idCardsService = BennuSpringContextHelper.getBean(SantanderIdCardsService.class);
-        Set<User> users = Bennu.getInstance().getUserSet();
+    public void runTask() {
+        Bennu.getInstance().getUserSet().stream()
+                .filter(u -> u.getCurrentSantanderEntry() == null).forEach(this::requestCard);
+    }
 
-        for (User user: users) {
-            List<RegisterAction> userActions = idCardsService.getPersonAvailableActions(user);
+    public void requestCard(User user) {
+        waitFlag = false;
+        FenixFramework.atomic(() -> {
             try {
-                if (userActions.contains(RegisterAction.NOVO)) {
-                    idCardsService.createRegister(user, RegisterAction.NOVO, "automatic card generation");
-                } else if (userActions.contains(RegisterAction.RENU)) {
-                    idCardsService.createRegister(user, RegisterAction.RENU, "automatic card generation");
-                }
-                taskLog("Requested card for user %s%n", user.getUsername());
-                Thread.sleep(5000);
-            } catch (SantanderValidationException se) {
-                taskLog("User %s cant request a card because: %s%n", user.getUsername(), se.getMessage());
-            } catch (RuntimeException e) {
-                taskLog("Couldn't request card for user %s%n", user.getUsername());
-                Thread.sleep(5000);
+                SantanderEntry createRegister =
+                        idCardsService.createRegister(user, RegisterAction.NOVO, "First card automatic request");
+                idCardsService.sendRegister(user, createRegister);
+                logger.debug("Created card with success for user {} (current SantanderEntry: {})", user.getUsername(),
+                        user.getCurrentSantanderEntry().getExternalId());
+                waitFlag = true;
+            } catch (SantanderCardNoPermissionException scnpe) {
+                logger.debug("No permission to request card for user {}", user.getUsername());
+            } catch (SantanderMissingInformationException smie) {
+                logger.debug("User {} has missing information: {}", user.getUsername(), smie.getMessage());
+                notifyMissingInformation(user, smie.getMessage());
+            } catch (SantanderValidationException sve) {
+                logger.debug("Error generating card for {} (current SantanderEntry: {}): {}", user.getUsername(),
+                        user.getCurrentSantanderEntry() == null ? "null" : user.getCurrentSantanderEntry().getExternalId(),
+                        sve.getMessage());
+                waitFlag = true;
+            } catch (Throwable t) {
+                logger.error(String.format("Failed for user %s (current SantanderEntry: %s)", user.getUsername(),
+                        user.getCurrentSantanderEntry() == null ? "null" : user.getCurrentSantanderEntry().getExternalId()), t);
+                waitFlag = true;
             }
+        });
+        if (waitFlag) {
+            sleep();
+        }
+    }
+
+    private void notifyMissingInformation(User user, String errors) {
+        Locale locale = user.getProfile().getPreferredLocale();
+        CardNotifications.notifyMissingInformation(user, idCardsService.getErrorMessage(locale, errors));
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(waitTime);
+        } catch (InterruptedException e) {
         }
     }
 }
