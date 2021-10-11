de3e7f40ab27d7af4517b26fe72811447f5e3e83
tiagoppinho <tiago.m.pinho@tecnico.ulisboa.pt>
2021-10-11 14:53:03 +0100

    Refactor cron task to update santander cards state. Minor improvements. IDC-86 #resolve

---
+++ src/main/java/org/fenixedu/idcards/tasks/UpdateSantanderEntriesStateTask.java
@@ -0,0 +1,61 @@
+package org.fenixedu.idcards.tasks;
+
+import org.fenixedu.bennu.core.domain.Bennu;
+import org.fenixedu.bennu.core.domain.User;
+import org.fenixedu.bennu.scheduler.CronTask;
+import org.fenixedu.bennu.scheduler.annotation.Task;
+import org.fenixedu.bennu.spring.BennuSpringContextHelper;
+import org.fenixedu.idcards.domain.SantanderCardState;
+import org.fenixedu.idcards.domain.SantanderEntry;
+import org.fenixedu.idcards.service.SantanderIdCardsService;
+
+/**
+ * @author Tiago Pinho
+ */
+
+@Task(englishTitle = "Update users santander entry state cron task", readOnly = true)
+public class UpdateSantanderEntriesStateTask extends CronTask {
+
+    private final SantanderIdCardsService service = BennuSpringContextHelper.getBean(SantanderIdCardsService.class);
+
+    /**
+     * Time in ms between santander webservice requests.
+     * Necessary to avoid too many followed requests, causing an error in the invoked webservice.
+     */
+    private final int WEBSERVICE_WAIT_TIME = 500;
+
+    @Override
+    public void runTask() throws Exception {
+        Bennu.getInstance().getUserSet().stream()
+                .filter(user -> user.getCurrentSantanderEntry() != null)
+                .forEach(this::updateSantanderEntryState);
+    }
+
+    private void updateSantanderEntryState(final User user) {
+        final SantanderEntry entry = user.getCurrentSantanderEntry();
+        final SantanderCardState state = entry.getState();
+
+        switch (state) {
+            case IGNORED:
+            case ISSUED:
+            case EXPIRED:
+            case DELIVERED:
+            case WAITING_INFO:
+                // Skip these states, there is nothing to update to.
+                break;
+            default:
+                // Update user entry state.
+                service.getOrUpdateState(user);
+                // Sleep using the webservice wait time.
+                sleep();
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(WEBSERVICE_WAIT_TIME);
+        } catch (final InterruptedException ignored) {
+        }
+    }
+
+}
